ea24ed21cb6d13d1f241ee9a24b1a6ade5699d28
Brody Broughton <broughton.brody07@gmail.com>
2024-09-12 14:40:06 -0600

    2.0

--- skibidi-cipher.py
+++ skibidi-cipher.py
@@ -1,46 +1,67 @@
 def skibidi():
-    table = {
-        A: Ski
-        B: bi
-        C: di
-        D:rizz
-        E:livdun
-        F:cap
-        G: fanum
-        H:bbygronk
-        I:hi
-        J:bomboclat
-        K:ohio
-        L:gyat
-        M:alpha
-N:beta
-O:sigma
-P:gas
-Q:male
-R:topg
-S:blud
-T: Gyatt
-U:edge
-V:lowk
-W:deadahh
-X:fire
-Y:goober
-Z:bussing
-1:nonchalant
-2:dreadhead
-3:drake
-4:grimace
-5:lightskin
-6:thuggshaker
-7:gta6
-8:mewing
-9:21
-0:uwu
-.:pog
--:glizzy
-_:nah
-?: sus
-!: imposter
-$: bands
 
+    table = {
+        'a':'ski',
+        'b':'bi',
+        'c':'di',
+        'd':'rizz',
+        'e':'livdun',
+        'f':'cap',
+        'g':'fanum',
+        'h':'bbygronk',
+        'i':'hi',
+        'j':'bomboclat',
+        'k':'ohio',
+        'l':'gyat',
+        'm':'alpha',
+        'n':'beta',
+        'o':'sigma',
+        'p':'gas',
+        'q':'male',
+        'r':'topg',
+        's':'blud',
+        't':'gyatt',
+        'u':'edge',
+        'v':'lowk',
+        'w':'deadahh',
+        'x':'fire',
+        'y':'goober',
+        'z':'bussing',
+        '1':'nonchalant',
+        '2':'dreadhead',
+        '3':'drake',
+        '4':'grimace',
+        '5':'lightskin',
+        '6':'thuggshaker',
+        '7':'gta6',
+        '8':'mewing',
+        '9':'21',
+        '0':'uwu',
+        '.':'pog',
+        '-':'glizzy',
+        "'":'nah',
+        '?':'sus',
+        '!':'imposter',
+        ',':'bands',
+        ' ':'galaxyg'
     }
+
+    inp = str(input('Enter the string you would like encrypted: ')).lower()
+
+    ls = []
+
+    for i in inp:
+        
+        ls.append(i)
+
+    newStr = ''
+
+    while len(ls) > 0:
+
+        cur = table[ls[0]]
+
+        newStr += cur
+
+        ls.pop(0)
+    print(newStr)
+skibidi()
