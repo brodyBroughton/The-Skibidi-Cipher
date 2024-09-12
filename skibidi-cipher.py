0fb400410acc0a126151e330216d71129edc2f18
Brody Broughton <broughton.brody07@gmail.com>
2024-09-12 14:13:37 -0600

    1.0

--- skibidi-cipher.py
+++ skibidi-cipher.py
@@ -1,2 +1,46 @@
-def test():
-  
+def skibidi():
+    table = {
+        A: Ski
+        B: bi
+        C: di
+        D:rizz
+        E:livdun
+        F:cap
+        G: fanum
+        H:bbygronk
+        I:hi
+        J:bomboclat
+        K:ohio
+        L:gyat
+        M:alpha
+N:beta
+O:sigma
+P:gas
+Q:male
+R:topg
+S:blud
+T: Gyatt
+U:edge
+V:lowk
+W:deadahh
+X:fire
+Y:goober
+Z:bussing
+1:nonchalant
+2:dreadhead
+3:drake
+4:grimace
+5:lightskin
+6:thuggshaker
+7:gta6
+8:mewing
+9:21
+0:uwu
+.:pog
+-:glizzy
+_:nah
+?: sus
+!: imposter
+$: bands
+
+    }
